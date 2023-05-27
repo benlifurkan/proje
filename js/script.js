c855de990bca384be26df1c0e193a5e8f46b09ed
Furkan Benli <62209524+benlifurkan@users.noreply.github.com>
2023-05-27 18:53:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(".menu-icon").click(function(){
+
+    $(".mobile-nav").css("display","block");
+
+    $(".mobile-nav").css("opacity","1");
+
+});
+
+
+
+$(".close-button").click(function(){
+
+    $(".mobile-nav").css("display","none");
+
+});
