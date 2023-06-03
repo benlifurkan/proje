93cbacf2b5abf7b6e215556e35146823ed358a30
Furkan Benli <62209524+benlifurkan@users.noreply.github.com>
2023-06-04 02:02:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-$(".menu-icon").click(function(){
+$(document).ready(function(){
 
-    $(".mobile-nav").css("display","block");
+    $(".menu-icon").click(function(){
 
-    $(".mobile-nav").css("opacity","1");
+        $(".mobile-nav").css("display","block");
 
-});
+        $(".mobile-nav").css("opacity","1");
+
+    });
 
 
 
-$(".close-button").click(function(){
+    $(".close-button").click(function(){
 
-    $(".mobile-nav").css("display","none");
+        $(".mobile-nav").css("display","none");
+
+    });
+
+
+            
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500){
+            $('.button').fadeIn();
+        }
+        else{
+            $('.button').fadeOut();
+        }
+    });
+
+    $('.button').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 
 });
